e3172271040804af1e47a742463ff64c538d4fdb
suresh9271 <56485375+suresh9271@users.noreply.github.com>
2019-10-13 20:25:49 +0530

    Delete InvoiceTest.java

--- Cucumber3/test/com/invoice/InvoiceTest.java
+++
@@ -1,7 +0,0 @@
-package com.invoice;
-import org.junit.runner.RunWith;
-import cucumber.junit.Cucumber;
-@RunWith(Cucumber.class)
-@Cucumber.Options(format={"json:target/cucumber.json"})
-public class InvoiceTest {
-}
